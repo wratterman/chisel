882cdca3abb8300ac1d0cf6c54e10a376c513c42
wratterman <will.ratterman@gmail.com>
2017-05-31 13:37:07 -0600

    transfered read message to array to parse through. translated first header to html

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,9 +1,14 @@
-# require './my_input.markdown'
-
 class Chisel
 
   def read_markdown
-    File.read('./my_input.markdown')
+    new_text = File.read('./my_input.markdown')
+    new_text = new_text.split("\n")
+    new_text.delete("")
+    p new_text
+  end
+
+  def translate_header
+    translated = read_markdown[0].sub("# ", "<h1>") + "</h1>"
   end
 
 end
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -13,14 +13,19 @@ class ChiselTest < Minitest::Test
 
   def test_it_will_read_what_is_in_my_input_file
     a = Chisel.new
-    expected = "# My Life in Desserts
-
-## Chapter 1: The Beginning
-
-\"You just *have* to try the cheesecake,\" he said. \"Ever since it appeared in
-**Food & Wine** this place has been packed every night.\"
-"
+    expected = ["# My Life in Desserts",
+ "## Chapter 1: The Beginning",
+ "\"You just *have* to try the cheesecake,\" he said. \"Ever since it appeared in",
+ "**Food & Wine** this place has been packed every night.\""]
     actual = a.read_markdown
+
+    assert_equal expected, actual
+  end
+
+  def test_it_will_add_html_h1_header_to_first_sentence
+    a = Chisel.new
+    expected = "<h1>My Life in Desserts</h1>"
+    actual = a.translate_header
 
     assert_equal expected, actual
   end
